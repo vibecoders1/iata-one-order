339ffb7842bf0de676423585f26985b9cca863ce
imdadareeph <vibecoders1@gmail.com>
2025-09-09 03:49:26 +0400

    feat: (one order) initial commit
     initial commit

---
+++ src/main/java/com/ia/iata/XsdToJavaApplication.java
@@ -0,0 +1,13 @@
+package com.ia.iata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsdToJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XsdToJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ia/iata/XsdToJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ia.iata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XsdToJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
